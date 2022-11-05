0230729b3a363d33554021745c2de4d073e9883b
ToshaBY <anton.rybakou@gmail.com>
2022-11-05 22:09:37 +0300

    feat: eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-function App() {
-    return <div>Hello world</div>
-}
+import { Header } from 'components';
+
+const App = (): ReactElement => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
